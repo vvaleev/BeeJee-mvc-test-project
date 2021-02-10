d8fbd62d566e9195860d3e0c34dab57e427921ad
Vadim Valeev <vvaleev95@mail.ru>
2021-02-10 18:06:25 +0500

    The main page has been prepared

--- public/index.php
+++ public/index.php
@@ -1,4 +1,29 @@
 <?php use System\View; ?>
 <?php View::renderView('header') ?>
-
+<div class="col-xs-12">
+    <div class="text-center">
+        <h1>Тестовое задание "Менеджер задач"</h1>
+        <p class="text-info">Ниже будет рыбный текст, т.к. пока для данной страницы нет необходимой информации.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
+            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
+    </div>
+</div>
 <?php View::renderView('footer') ?>
