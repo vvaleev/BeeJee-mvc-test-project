9c64321c898d980e6d77384d95abf9aa9f415f65
Vadim Valeev <vvaleev95@mail.ru>
2021-02-10 14:49:42 +0500

    Added jquery library

--- views/header/template.php
+++ views/header/template.php
@@ -8,6 +8,7 @@
     <?php Page::renderCss('fonts'); ?>
     <?php Page::renderCss('bootstrap.min'); ?>
     <?php Page::renderCss('style'); ?>
+    <?php Page::renderJs('jquery-3.3.1.min'); ?>
     <?php Page::renderJs('bootstrap.min'); ?>
     <?php Page::renderJs('script'); ?>
 
