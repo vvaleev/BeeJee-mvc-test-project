d86f679cd6eddba6048cf1e8cbde2ae783562ba3
Vadim Valeev <vvaleev95@mail.ru>
2021-02-17 21:45:26 +0500

    task.list view: admin access

--- views/task.list/template.php
+++ views/task.list/template.php
@@ -14,18 +14,37 @@
             .setOption('pageLength', 3)
             .setOption('paging', true)
             .setOption('searching', true)
-            .setOption('ordering', true);
-            // .setOption('order', [[0, 'desc']]);
-
-        Task.List.setColumns([
-            {title: 'Название задачи'},
-            {title: 'Описание задачи'},
-            {title: 'Автор задачи'},
-            {title: 'E-mail автора задачи'},
-            {title: 'Статус задачи'},
-        ]);
+            .setOption('ordering', true)
+            .setOption('order', [[0, 'desc']]);
 
         Task.List.getData(function (data) {
+            var columns = [
+                {title: 'ID'},
+                {title: 'Название задачи'},
+                {title: 'Описание задачи'},
+                {title: 'Автор задачи'},
+                {title: 'E-mail автора задачи'},
+                {title: 'Статус задачи'},
+            ];
+
+            if (Array.isArray(data) && typeof data[0] === 'object') {
+                if (data[0][Object.keys(data[0]).length - 1] === 'operations') {
+                    columns.push({title: 'Операции'});
+
+                    for (var key in data) {
+                        if (data.hasOwnProperty(key)) {
+                            data[key].pop();
+                            data[key].push(
+                                '<a href="/task/remove/' + data[key][0] + '/">Удалить</a>' +
+                                '&nbsp;' +
+                                '<a href="/task/edit/' + data[key][0] + '/">Редактировать</a>'
+                            );
+                        }
+                    }
+                }
+            }
+
+            Task.List.setColumns(columns);
             Task.List.setData(data);
             Task.List.renderData();
         });
