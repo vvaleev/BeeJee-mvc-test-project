e774e659afcb97fd49db0b342b44494d8e59d587
Vadim Valeev <vvaleev95@mail.ru>
2021-02-17 23:30:39 +0500

    main page: added views..

--- public/index.php
+++ public/index.php
@@ -1,29 +1,8 @@
 <?php use System\View; ?>
 <?php View::renderView('header') ?>
 <div class="col-xs-12">
-    <div class="text-center">
-        <h1>Тестовое задание "Менеджер задач"</h1>
-        <p class="text-info">Ниже будет рыбный текст, т.к. пока для данной страницы нет необходимой информации.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at debitis dicta earum modi natus nihil
-            placeat sit voluptates. Dicta eaque minima modi nam nemo obcaecati voluptas. Quia, tenetur vitae.</p>
-    </div>
+    <?php View::renderView('task.list') ?>
+    <?php View::renderView('task.add') ?>
+    <?php View::renderView('task.edit') ?>
 </div>
 <?php View::renderView('footer') ?>
